4081baa351455bb44d6b5915c4cff17aeb6e1923
Al-bann <bani_mucaj@hotmail.com>
2025-04-14 15:19:24 +0200

    Fix import path for button

--- src/CuttingOptimizer.jsx
+++ src/CuttingOptimizer.jsx
@@ -1,9 +1,9 @@
 
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Table, TableHeader, TableBody, TableRow, TableCell } from "@/components/ui/table";
+import { Button } from "./components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Input } from "./components/ui/input";
+import { Table, TableHeader, TableBody, TableRow, TableCell } from "./components/ui/table";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CuttingOptimizer from "./CuttingOptimizer";
+import CuttingOptimizer from "./components/CuttingOptimizer";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
