0b9a61fa87c472f0c3bc176d61f5c29c52bd4876
Al-bann <bani_mucaj@hotmail.com>
2025-04-14 16:03:26 +0200

    Version Final me zbritje 6cm, permbledhje te shufrave dhe PDFte plote

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CuttingOptimizer from "./CuttingOptimizer";
 
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CuttingOptimizer />
